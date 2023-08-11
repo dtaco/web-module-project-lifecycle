fafb3966264ebd7d799eb3e8eddc9ec41146b508
David Tacoronte <davidtacoronte@Davids-MBP.attlocal.net>
2023-08-11 10:53:34 -0400

    started project and building out state, helper functions, and CDM.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      todos: [],
+    }
+  }
+
+  fetchAllTodos = () => {
+
+  }
+
+  componentDidMount() {
+    
+  }
+
   render() {
     return null
   }
